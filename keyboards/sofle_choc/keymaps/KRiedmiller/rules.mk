31522fceced952f322eedab829a34d9f78e79c58
Kai Riedmiller <kai.riedmiller@web.de>
2024-01-17 20:02:49 +0100

    my sofle-choc

---
+++ keyboards/sofle_choc/keymaps/KRiedmiller/rules.mk
@@ -0,0 +1,12 @@
+ENCODER_MAP_ENABLE = yes
+COMMAND_ENABLE = no # for space_cadet
+SPACE_CADET_ENABLE = no
+MOUSEKEY_ENABLE = yes # bigger size
+CAPS_WORD_ENABLE = yes
+WPM_ENABLE = yes
+
+# Reduce compiled size
+GRAVE_ESC_ENABLE = no
+LTO_ENABLE = yes
+CONSOLE_ENABLE = no
+MUSIC_ENABLE = no`
